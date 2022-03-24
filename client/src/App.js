1aa9d758bcc11214b7d4fcb275bba9be4ef4aa50
Jaime Salgado <jsalg10@gmail.com>
2022-03-24 10:32:11 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/navbar";
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <p> Hello World </p>
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div className="navbar_Container">
+      <ul>
+        <li>Menu</li>
+        <li>Home</li>
+        <li>Search</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
